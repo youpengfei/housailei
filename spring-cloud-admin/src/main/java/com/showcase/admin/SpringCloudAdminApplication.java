25ef7930ad9689936a9a2d3eb57a650587fd0e9c
pengfei.you <pengfei.you@nio.com>
2018-12-25 15:53:47 +0800

    醒目初始化

---
+++ spring-cloud-admin/src/main/java/com/showcase/admin/SpringCloudAdminApplication.java
@@ -0,0 +1,14 @@
+package com.showcase.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAdminApplication.class, args);
+    }
+
+}
+
